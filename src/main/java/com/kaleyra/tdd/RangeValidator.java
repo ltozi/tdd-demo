1e1d08b614720f76e9a12e2a99ead4ab40687c94
ltozi <luigi.toziani@kaleyra.com>
2020-03-24 15:41:11 +0100

    Some refactoring

--- src/main/java/com/kaleyra/tdd/RangeValidator.java
+++ src/main/java/com/kaleyra/tdd/RangeValidator.java
@@ -1,6 +1,11 @@
 package com.kaleyra.tdd;
 
 public class RangeValidator<Integer> implements Validator<Integer> {
+
+    public RangeValidator(Integer min, Integer max) {
+
+    }
+
     @Override
     public void validate(Integer str) throws IllegalArgumentException {
 
